233c7428d41a9c57ee23157be90ebea572227daf
sps992 <hello@samsedgeman.co.uk>
2023-06-15 21:57:20 +0100

    Adding preact and light/dark css variation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    config: { applyBaseStyles: false },
-  })]
+    config: {
+      applyBaseStyles: false
+    }
+  }), preact()]
 });
