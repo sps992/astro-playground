a46324a6d222cdfab935e4c49b8477a676cebf7d
sps992 <hello@samsedgeman.co.uk>
2023-07-02 21:49:16 +0100

    Adding Storyblok config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
 import tailwind from "@astrojs/tailwind";
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
+    vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+        components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+  },
+    }),
+    tailwind({
     config: {
       applyBaseStyles: false
     }
